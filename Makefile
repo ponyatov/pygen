e0954e097c185e59c6925446e137ee6cb81089c7
Dmitry Ponyatov <dponyatov@gmail.com>
2017-05-07 22:20:31 +0400

    + AST/Program classes, minimal .cpp autogen/build
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- Makefile
+++ Makefile
@@ -1,2 +1,6 @@
-log.log cpp.cpp: py.py
-	python py.py > log.log && tail $(TAIL) log.log
+log.log: ./exe.exe
+	./exe.exe >> $@ && tail $(TAIL) $@
+cpp.cpp: py.py
+	python py.py > log.log && tail $(TAIL) $@
+./exe.exe: cpp.cpp
+	$(CXX) -o $@ $^
--- py.py
+++ py.py
@@ -1,5 +1,29 @@
 log = open('log.log','w')
 cpp = open('cpp.cpp','w')
+print >>cpp,'// autogen: https://github.com/ponyatov/pygen'
 
-print >>log,log
-print >>cpp,cpp
+class AST:
+	# constructor
+	def __init__(self,V):
+		self.tag = self.__class__.__name__ ; self.val = V
+		self.nest = []
+	# dump
+	def __repr__(self): return self.dump()
+	def dump(self,depth=0):
+		S = '\n'+'\t'*depth+self.head()
+		for j in self.nest: S += j.dump(depth+1)
+		return S
+	def head(self): return '<%s:%s>'%(self.tag,self.val)
+	# codegen
+	def cpp(self):
+		C = '// %s\nint main(){}\n'%self.head()
+		for j in self.nest: C += j.cpp()
+		return C
+
+class Program(AST): pass
+
+prog = Program('Hello')
+
+print >>log,prog
+print >>cpp,prog.cpp()
+
